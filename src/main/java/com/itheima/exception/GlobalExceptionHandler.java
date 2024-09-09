01194f3c932eb2d76e967124a05f7b27892ca14b
itcast <2160475704@qq.com>
2024-09-09 17:58:49 +0800

    test...

--- src/main/java/com/itheima/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itheima/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ public class GlobalExceptionHandler {
     public Result ex(Exception ex){
         ex.printStackTrace();
         System.out.println(123);
+        System.out.println(234);
         return Result.error("对不起，操作失败，请联系管理员");
 
-    }
-}
+    }}
